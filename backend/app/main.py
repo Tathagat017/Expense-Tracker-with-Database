66bfe90d6e56e754f527af8bbd0e9f0b34cd1b11
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2025-07-10 20:03:17 +0530

    cors fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from app.db.database import Base,engine
 from app.routers import expenses
@@ -6,6 +7,14 @@ from app.routers import expenses
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Expense Tracker",description="Track your expenses")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(expenses.router)
 
